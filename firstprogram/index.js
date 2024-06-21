940c16e01a5dcc04a956f6814b43d4e5009a3007
alauddinDipu <alauddin.cucse@gmail.com>
2024-06-21 15:15:42 +0600

    First programt tested

---
+++ firstprogram/index.js
@@ -0,0 +1,12 @@
+var uName = "Alauddin Dipu";
+console.log(uName);
+function addNumber(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumber(20, 30);
+addNumber(20, "30");
+function sumNumber(nm1, nm2) {
+    console.log(nm1 + nm2);
+}
+sumNumber(20, 50);
+sumNumber(20, "3");
---
+++ firstprogram/index.ts
@@ -0,0 +1,19 @@
+let uName="Alauddin Dipu";
+console.log(uName);
+
+function addNumber(num1,num2){
+    console.log(num1+num2);
+}
+
+addNumber(20,30);
+addNumber(20,"30");
+
+
+
+function sumNumber(nm1:number,nm2:number){
+    console.log(nm1+nm2);
+}
+
+
+sumNumber(20,50);
+sumNumber(20,"3");
