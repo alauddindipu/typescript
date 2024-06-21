4f65b1534d6c17e21102c6c9281dc184a48fd669
alauddinDipu <alauddin.cucse@gmail.com>
2024-06-21 15:58:53 +0600

    build-in-type practice

---
+++ built-in-type/index.js
@@ -0,0 +1,9 @@
+var uId;
+var firstName;
+var lastName;
+var isActiveated;
+uId = 12;
+firstName = "Alauddin";
+lastName = "Dipu";
+isActiveated = true;
+console.log(uId);
---
+++ built-in-type/index.ts
@@ -0,0 +1,13 @@
+let uId : number;
+let firstName:string;
+let lastName:string;
+let isActiveated:boolean;
+
+uId=12;
+firstName="Alauddin";
+lastName="Dipu";
+isActiveated=true;
+
+console.log(uId);
+
+
